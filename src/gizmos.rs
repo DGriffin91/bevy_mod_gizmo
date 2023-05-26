8f5b29ed13468a66f7ff3ae4f3a3cd6fc2bb8420
Daniel Griffin <github@dgdigital.net>
2023-05-25 17:38:51 -0700

    merge #8434

--- src/gizmos.rs
+++ src/gizmos.rs
@@ -16,7 +16,6 @@ pub(crate) struct GizmoStorage {
     pub list_colors: Vec<ColorItem>,
     pub strip_positions: Vec<PositionItem>,
     pub strip_colors: Vec<ColorItem>,
-    pub in_use: bool,
 }
 
 pub type Gizmos<'s> = Deferred<'s, GizmoBuffer>;
